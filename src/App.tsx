3163247eda57b4428180e8171e9adc8f67f7f9ca
Alexey Poryadin <alexey.poryadin@gmail.com>
2023-06-15 23:10:04 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import CenteredLayout from '@/components/Layout/Centered';
+
+export default function App() {
+  return (
+    <CenteredLayout>
+      <h1>Hello, world!</h1>
+    </CenteredLayout>
+  );
+}
