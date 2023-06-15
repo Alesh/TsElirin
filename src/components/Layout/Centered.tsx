a867700cc05776c8a06323248d50529a033c3723
Alexey Poryadin <alexey.poryadin@gmail.com>
2023-06-15 23:10:04 +0300

    First commit

---
+++ src/components/Layout/Centered.tsx
@@ -0,0 +1,10 @@
+import { FC, PropsWithChildren } from 'react';
+import * as classes from './Centered.module.css';
+
+/// Layout with one centered view
+const CenteredLayout: FC<PropsWithChildren> = ({ children }) => (
+  <div className={classes.outer}>
+    <div className={classes.inner}>{children}</div>
+  </div>
+);
+export default CenteredLayout;
